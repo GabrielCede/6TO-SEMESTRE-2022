a0ffecf47b8817c2dba40abb88f95cf15d6521b4
GabrielCede <56653028+GabrielCede@users.noreply.github.com>
2022-10-15 03:56:43 +0200

    Add files via upload

--- 1erParcial/Practica-3/index.js
+++ 1erParcial/Practica-3/index.js
@@ -4,7 +4,7 @@
 const mongoose = require('mongoose');
 
 //La cadena de caracteres que me permite conectarme a mi nube en el servidor de MongoDB
-const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/test";
+const connectionURL= "mongodb+srv://EIGI:eigi1999@cluster0.yaxkm.mongodb.net/test";
 
 
 
@@ -48,6 +48,8 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
             nombre:String,
             nombre_Eq1:String,
             nombre_Eq2:String,
+            fecha:String,
+            hora:String,
             idpronostico: { type: mongoose.Types.ObjectId , ref:"pronostico" }
 
         });
@@ -65,7 +67,8 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
         const apostador1=  new apostador(
             {
                 nombre:"Edison Aguirre",
-                indent:0958564367
+                identificacion:0958564367
+                
                
             }
             );
@@ -76,7 +79,7 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
         const apostador2=  new apostador(
             {
                 nombre:"Carlos Palma",
-                indent:0958564485
+                identificacion:0958564485
                
             }
             );
@@ -87,7 +90,7 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
         const apostador3=  new apostador(
             {
                 nombre:"karla Cedeño",
-                indent:0958564123
+                identificacion:0958564123
                
             }
             );
@@ -98,7 +101,7 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
         const apostador4=  new apostador(
             {
                 nombre:"Gabriel Cedeño",
-                indent:0958156947
+                identificacion:0958156947
                
             }
             );
@@ -109,12 +112,74 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
         const apostador5=  new apostador(
             {
                 nombre:"Anette Moreno",
-                indent:0958156741
+                identificacion:0958156741
                
             }
             );
             //Guardo el usuario
         const apostador5Save=  await apostador5.save();
+    //#########################################################
+
+            //Pronostico1
+        const pronostico1=  new pronostico(
+            {
+                resultado_propuesto:"Pasto de Grama 2 Goles - Real Furcia 1 Gol ",
+                valor_apuesta:"20.00$"
+               
+            }
+            );
+            //Guardo el usuario
+        const pronostico1Save=  await pronostico1.save();
+
+        
+        //Pronostico2
+        const pronostico2=  new pronostico(
+            {
+                resultado_propuesto:"Minabo de Kiev 0 Goles - Vodka Juniors 1 Gol ",
+                valor_apuesta:"10.00$"
+               
+            }
+            );
+            //Guardo el usuario
+        const pronostico2Save=  await pronostico2.save();
+
+            //Pronostico3
+        const pronostico3=  new pronostico(
+            {
+                resultado_propuesto:"Aston Birra 1 Gol - Yayo Vallecano 1 Gol ",
+                valor_apuesta:"30.00$"
+               
+            }
+            );
+            //Guardo el usuario
+        const pronostico3Save=  await pronostico3.save();
+
+        //Pronostico4
+        const pronostico4=  new pronostico(
+            {
+                resultado_propuesto:"Minabo de Kiev 1 Gol - Vodka Juniors 1 Gol ",
+                valor_apuesta:"10.00$"
+               
+            }
+            );
+            //Guardo el usuario
+        const pronostico4Save=  await pronostico4.save();
+
+        //Pronostico5
+        const pronostico5=  new pronostico(
+            {
+                resultado_propuesto:"Pasto de Grama 5 Goles - Real Furcia 0 Gol ",
+                valor_apuesta:"50.00$"
+               
+            }
+            );
+            //Guardo el usuario
+        const pronostico5Save=  await pronostico5.save();
+
+
+
+
+
     //#########################################################
 
         //Datos de los encuentros deportivos
@@ -123,8 +188,10 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
 
         const encuentro_D1=  new encuentro_D(
             {
-                resultado_propuesto:"Pasto de Grama 2 Goles - Real Furcia 1 Gol ",
-                valor_apuesta:"20.00$"
+                nombre_Eq1:"Minabo de Kiev",
+                nombre_Eq2:"Vodka Juniors",
+                fecha:"20/09/2022",
+                hora:"3:00 PM"
                          
             }
             );
@@ -135,8 +202,10 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
 
         const encuentro_D2=  new encuentro_D(
             {
-                resultado_propuesto:"Minabo de Kiev 0 Goles - Vodka Juniors 1 Gol ",
-                valor_apuesta:"10.00$"
+                nombre_Eq1:"Aston Birra",
+                nombre_Eq2:"Yayo Vallecano",
+                fecha:"23/09/2022",
+                hora:"6:00 PM"
                          
             }
             );
@@ -147,16 +216,17 @@ const connectionURL= "mongodb+srv://EIGI:<eigi1999>@cluster0.yaxkm.mongodb.net/t
 
         const encuentro_D3=  new encuentro_D(
             {
-                resultado_propuesto:"Aston Birra 1 Gol - Yayo Vallecano 1 Gol ",
-                valor_apuesta:"30.00$"
+                nombre_Eq1:"Pasto de Grama",
+                nombre_Eq2:"Real Furcia",
+                fecha:"27/09/2022",
+                hora:"3:00 PM",
                          
             }
             );
             //Guardar el encuentro
-        const encuentro_D23ave=  await encuentro_D3.save();
+        const encuentro_D3Save=  await encuentro_D3.save();
+
         
-
-
 
         //#endregion
 
